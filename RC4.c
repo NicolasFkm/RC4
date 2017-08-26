9de44d47809be37e8513554be9550b351b07fa7a
Nicolas Fukuyama <nicolas.fukuyama@hotmail.com>
2017-08-26 15:22:25 -0300

    RC4 Criptografia

---
+++ RC4.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+void ksa(unsigned char state[], unsigned char key[], int len){
+   int i,j=0,t;
+
+   for (i=0; i < 256; ++i)
+      state[i] = i;
+   for (i=0; i < 256; ++i) {
+      j = (j + state[i] + key[i % len]) % 256;
+      t = state[i];
+      state[i] = state[j];
+      state[j] = t;
+   }
+}
+
+void prga(unsigned char *s, unsigned char *Data, unsigned long Len){
+    int i = 0;
+    int j = 0;
+    int t = 0;
+    unsigned long k = 0;
+    unsigned char tmp;
+    for(k=0;k<Len;k++)
+    {
+        i=(i+1)%256;
+        j=(j+s[i])%256;
+        tmp = s[i];
+        s[i] = s[j];
+        s[j] = tmp;
+        t=(s[i]+s[j])%256;
+        Data[k] ^= s[t];
+    }
+}
+
+int main(){
+    unsigned char state[256], key[25], texto[50];
+    printf("\nTEXTO(max. 50 caracteres): ");
+    fgets(texto, 50, stdin);
+    fflush(stdin);
+    printf("\nCHAVE(max. 25 caracteres): ");
+    fgets(key, 25, stdin);
+    fflush(stdin);
+
+    int textoLength=strlen(texto), chaveLength=strlen(key) ,idx;
+
+
+    ksa(state,key,chaveLength);
+    printf("\n----------TEXTO ORIGINAL-----------\n");
+    for (idx=0; idx < textoLength; idx++){
+        printf("%c ",texto[idx]);
+    }
+
+    prga(state,texto,textoLength);
+
+    printf("\n--------TEXTO CRIPTOGRAFADO--------\n");
+    for (idx=0; idx < textoLength; idx++){
+        printf("%c ",texto[idx]);
+    }
+
+    ksa(state,key,chaveLength);
+    prga(state,texto,textoLength);
+
+    printf("\n-------TEXTO DESCRIPTOGRAFADO------\n");
+    for (idx=0; idx < textoLength; idx++){
+        printf("%c ",texto[idx]);
+    }
+   return 0;
+}
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # RC4
 RC4 é um algoritmo de criptografia que não utiliza calculos complexos, funciona basicamente por meio de permutações.
 
-O algoritmo utiliza basicamente dois algoritmos, o algoritmo KSA ou Key Scheduling Algorithm
+## Functions
+O algoritmo utiliza basicamente dois algoritmos: o algoritmo KSA ou *Key-Scheduling Algorithm* que inicializa um array de estado preenchendo-o com valores de 0 a 255, depois são feitas diversas permutações, dentro de um laço de 0 a 255, baseando-se em valores do array no qual a chave está contida; e o algoritmo PRGA ou **Pseudo-Random Generation Algorithm** que inicializa variáveis com 0, depois inicializa o loop de 0 ate o tamanho do array da chave,são feitas trocas dentro do array de estado, e no passo final do laço realiza-se um XOR entre o array de estado e o array do texto enviado.
 
 
